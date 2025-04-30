bc39dc2f28fd13659e26ee1baf198457d5b7bbbd
Maiii00 <maykrop0717@gmail.com>
2025-04-30 16:45:11 +0800

    準備部署

--- chat-frontend/src/app/app.config.ts
+++ chat-frontend/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideToastr } from 'ngx-toastr';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
