259b4df10638210101280f054370bacc47df17e2
Maiii00 <maykrop0717@gmail.com>
2025-05-01 10:27:18 +0800

    準備部署

--- chat-frontend/src/environments/environment.prod.ts
+++ chat-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://chat-web-production-e809.up.railway.app/api',
-    wsUrl: 'https://chat-web-production-e809.up.railway.app/ws'
+    apiBaseUrl: '/api',
+    wsUrl: '/ws'
 };
   
